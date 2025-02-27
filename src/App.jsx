3594a6d1f1cee8fa0b273f70943a4314fec6521f
SIVAKUMAR-G-S <sivakumar25828@gmail.com>
2025-02-27 21:10:58 +0530

    netflix

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,43 @@
-import React, { useEffect, useRef } from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
-import Home from './Pages/Home/Home';
-import Login from './Pages/Login/Login';
-import Player from './Pages/Player/Player';
-import { onAuthStateChanged } from 'firebase/auth';
-import { auth } from './firebase';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React, { useEffect, useRef } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
+import Player from "./Pages/Player/Player";
+import { getAuth, onAuthStateChanged } from "firebase/auth"; 
+import { auth } from "./firebase"; 
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   const navigate = useNavigate();
-  const isFirstRender = useRef(true); 
+  const isFirstRender = useRef(true);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (isFirstRender.current) {
-        isFirstRender.current = false; 
+        isFirstRender.current = false;
         return;
       }
 
       if (user) {
         toast.success("Logged In");
-        navigate('/');
+        navigate("/");
       } else {
         toast.info("Logged Out");
-        navigate('/login');
+        navigate("/login");
       }
     });
 
-    return () => unsubscribe(); 
-  }, []); 
+    return () => unsubscribe();
+  }, [navigate]);
 
   return (
     <div>
-      <ToastContainer theme='dark' />
+      <ToastContainer theme="dark" />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/player/:id' element={<Player />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/player/:id" element={<Player />} />
       </Routes>
     </div>
   );
